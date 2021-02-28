a75791ba471f2c679464933ffb171233ff4548ec
Martin <martinjesu.joseph@gmail.com>
2021-03-01 00:32:16 +0530

    Base folder structure for Main page & Settings page

--- src/components/poc/poc.jsx
+++ src/components/poc/poc.jsx
@@ -1,11 +1,33 @@
 import React, { Fragment, useState } from 'react';
+import DataTable from 'react-data-table-component';
+
 import './poc.css';
+
+const data = [{ id: 1, title: 'Conan the Barbarian', year: '1982' }];
+const columns = [
+  {
+    name: 'Title',
+    selector: 'title',
+    sortable: true,
+  },
+  {
+    name: 'Year',
+    selector: 'year',
+    sortable: true,
+    right: true,
+  },
+];
 
 export default function POC(props) {
 
     return (
         <div id="poc-container">
             <span>POC Component</span>
+            <DataTable
+                title="Arnold Movies"
+                columns={columns}
+                data={data}
+            />
         </div>
     )
 }
